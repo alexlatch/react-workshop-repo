5c4af3dac561c8e21c87521534adecd342df5ffb
“alexlatch” <annalatiolais1@icloud.com>
2023-09-04 23:11:04 -0500

    select a featured puppy tab

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,28 @@ import { useState } from 'react'
 import {puppyList} from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [puppies, setPuppies] = useState(puppyList) 
-  console.log (puppies)
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
     <>
       <div>
       { 
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={()=>{ setFeatPupId(puppy.id)}}
+          key={puppy.id}>{puppy.name}</p>
         })
       }
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
     </>
   )
