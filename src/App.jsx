45fbab6e6062d2fb608759d05a08feb63d3da190
“alexlatch” <annalatiolais1@icloud.com>
2023-09-04 22:39:18 -0500

    useState tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
+import { useState } from 'react'
 import {puppyList} from './data.js'
 
 function App() {
   const [count, setCount] = useState(0)
-  console.log(puppyList)
-
+  const [puppies, setPuppies] = useState(puppyList) 
+  console.log (puppies)
   return (
     <>
       <div>
-        
+      { 
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
       </div>
     </>
   )
